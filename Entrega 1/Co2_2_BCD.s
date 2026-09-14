afdc54ac4fa98211fb973cdd3b12380a3e514294
William12092002 <125163887+William12092002@users.noreply.github.com>
2026-09-14 16:11:40 -0500

    Update Co2_2_BCD.s

--- Entrega 1/Co2_2_BCD.s
+++ Entrega 1/Co2_2_BCD.s
@@ -3,9 +3,10 @@
 .data
 # 1. Lista de números a probar
 lista: 
-    .word -123456      # Prueba 1
+    .word 1200000 #rueba 1
     .word -98765         # Prueba 2 
-    .word 0              # Prueba 3 
+    .word 0              # Prueba 3
+	.word 2147483647
 
 # 2. Arreglo de potencias de 10 
 po10:
@@ -32,32 +33,33 @@ _start:
     
 	
 	addi sp, zero, 1024 #pongo el puntero de stack lejos de los valores guardados
-	addi gp, zero, 216 # posición de "lista" en la memoria 
+	#addi gp, zero, 216 # posición de "lista" en la memoria 
+	la gp, lista
 	
     addi s0, zero, 0     # Índice del bucle de pruebas (i = 0)
-    addi s1, zero, 3     # Cantidad de pruebas a realizar
+    addi s1, zero, 4     # Cantidad de pruebas a realizar
     add s2, gp, zero     # Puntero a lista
-    addi s3, gp, 12      # Puntero a las potencias
-    addi s4, gp, 52      # Puntero de escritura 
+    la s3, po10          # Puntero a las potencias
+    la s4, bcd_out       # Puntero de escritura 
 
-test_loop:
-    beq s0, s1, end_main # Si s0 == s3, terminar programa
+	test_loop:
+    	beq s0, s1, end_main # Si s0 == s3, terminar programa
 
-    # Cargar argumentos para la función
-    lw a0, 0(s2)         # Número actual de la lista
-    add a1, s3, zero     # Puntero a potencias
-    add a2, s4, zero     # Puntero a bcd_out
+    	# Cargar argumentos para la función
+    	lw a0, 0(s2)         # Número actual de la lista
+    	add a1, s3, zero     # Puntero a potencias
+    	add a2, s4, zero     # Puntero a bcd_out
 
-    # Llamar a la función
-    jal ra, ca2BCD
+    	# Llamar a la función
+    	jal ra, ca2BCD
 
-    # Avanzar punteros e índice para la siguiente prueba
-    addi s2, s2, 4       # Siguiente número en la lista
-    addi s0, s0, 1       # s0++
-    jal zero, test_loop  # Repetir
+    	# Avanzar punteros e índice para la siguiente prueba
+    	addi s2, s2, 4       # Siguiente número en la lista
+    	addi s0, s0, 1       # s0++
+    	jal zero, test_loop  # Repetir
 
-end_main:
-    jal zero, end_main   # Bucle infinito para finalizar programa
+	end_main:
+    	jal zero, end_main   # Bucle infinito para finalizar programa
 
 
 
@@ -87,48 +89,46 @@ ca2BCD:
     jal zero, set
 	
 	es_positivo:
-    # Caso Positivo: Guardar 0
-    addi t1, zero, 0     
-    sw t1, 0(s5)         # Guardar 0 en el primer word
-    addi s5, s5, 4       # Avanzar puntero para los dígitos
+    	# Caso Positivo: Guardar 0
+    	addi t1, zero, 0     
+    	sw t1, 0(s5)         # Guardar 0 en el primer word
+    	addi s5, s5, 4       # Avanzar puntero para los dígitos
 	
 	
 
-set:
-    add s3, a1, zero     # s3 = puntero de lectura de potencias
-    addi s4, zero, 10    # s4 = 10 iteraciones 
+	set:
+    	add s3, a1, zero     # s3 = puntero de lectura de potencias
+    	addi s4, zero, 10    # s4 = 10 iteraciones 
 
-set_loop:
-    beq s4, zero, fin
-    lw t0, 0(s3)         # Cargar potencia actual
-    addi s3, s3, 4       # Avanzar puntero de potencias
-    addi s4, s4, -1      # Decrementar contador
-    addi t1, zero, 0     # Inicializar dígito actual en 0
+	set_loop:
+    	beq s4, zero, fin
+    	lw t0, 0(s3)         # Cargar potencia actual
+    	addi s3, s3, 4       # Avanzar puntero de potencias
+    	addi s4, s4, -1      # Decrementar contador
+    	addi t1, zero, 0     # Inicializar dígito actual en 0
 
-sub_loop:
-    blt t6, t0, saveD # Si (numero < potencia), terminar restas
-    sub t6, t6, t0       # numero = numero - potencia
-    addi t1, t1, 1       # digito++
-    jal zero, sub_loop   
+	sub_loop:
+    	blt t6, t0, saveD # Si (numero < potencia), terminar restas
+    	sub t6, t6, t0       # numero = numero - potencia
+    	addi t1, t1, 1       # digito++
+    	jal zero, sub_loop   
 
-saveD:
-    # Escribo el dígito calculado en memoria
-    sw t1, 0(s5)
-    addi s5, s5, 4       # Avanzar puntero de escritura en 1 word
-    jal zero, set_loop   # Siguiente potencia
+	saveD:
+    	# Escribo el dígito calculado en memoria
+    	sw t1, 0(s5)
+    	addi s5, s5, 4       # Avanzar puntero de escritura en 1 word
+    	jal zero, set_loop   # Siguiente potencia
 
-fin:
+	fin:
     
-    lw s5, 0(sp)
-    lw s4, 4(sp)
-    lw s3, 8(sp)
-    lw s2, 12(sp)
-    lw ra, 16(sp)
-    addi sp, sp, 20
+    	lw s5, 0(sp)
+    	lw s4, 4(sp)
+    	lw s3, 8(sp)
+    	lw s2, 12(sp)
+    	lw ra, 16(sp)
+    	addi sp, sp, 20
 
-    jr ra # Retorno 
-			
-		 	
+    	jr ra # Retorno 
 	
 	
 		
