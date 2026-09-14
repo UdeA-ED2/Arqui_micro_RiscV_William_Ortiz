31976e6858b3caaed4dcf8d42a63a6fbf52fffbc
William12092002 <125163887+William12092002@users.noreply.github.com>
2026-09-14 17:39:35 -0500

    Update bcd2float_float2bcd.s

--- Entrega 2/bcd2float_float2bcd.s
+++ Entrega 2/bcd2float_float2bcd.s
@@ -1,1 +1,35 @@
+.global _start
 
+.data
+
+pruebas:
+
+	.word 2
+	.word 4
+	.word 6
+	.word 9
+	.word 42 #el 42 representa el asterisco en ASCCI, será la bandera que avise cuando el numero acabó 
+	
+.text
+_start:
+
+	set:	
+		addi s0, zero, 42
+		la gp, pruebas
+		beq t0, s0, fin
+	
+	save_num:
+		lw t0, 0(gp)
+		addi gp, gp, 4
+		add t1, zero, t0
+		addi t2, zero, 10
+		
+		
+	sucev_sum:
+		beq t2, zero, set
+		addi t2, t2, -1
+		addi t1, t1, 10
+		addi t3, t3, t1
+		j sucev_sum
+	   
+	
